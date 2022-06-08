67474982294cbcd45dbf1d764522bd302125d7c2
KevinGamer790 <colin@mcgregor.us>
2022-06-08 17:31:18 -0600

    poop!

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -74,7 +74,6 @@ const sumfetch = async (args: string[]): Promise<string> => {
                                   爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
                                    -----------
                                    CONTACT 
-                                   <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
                                    <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
                                    -----------
                                    DONATE 
