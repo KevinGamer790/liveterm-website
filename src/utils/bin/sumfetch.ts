e5ef86850f574e87c5bc8720c02d7fd525b0f9c2
KevinGamer790 <colin@mcgregor.us>
2022-06-10 22:16:31 -0600

    Update sumfetch.ts

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -75,7 +75,7 @@ const sumfetch = async (args: string[]): Promise<string> => {
                                    -----------
                                    CONTACT 
                                    <u><a href= "https://twitter.com/kevinawrf" target="https://twitter.com/kevinawrf">@kevinawrf on twitter</a></u> | use the 'twitter' command
-                                  <i class="fa-brands fa-discord"></i> <u><a href="https://discord.gg/zNFxq4QEy2" target="https://discord.gg/zNFxq4QEy2">Californya Discord Server</a></u> | use the 'discord' command
+                                   <u><a href="https://discord.gg/zNFxq4QEy2" target="https://discord.gg/zNFxq4QEy2">Californya Discord Server</a></u> | use the 'discord' command
                                    -----------
                                    DONATE 
                                    <u><a href="${config.donate_urls.paypal}" target="_blank">${config.donate_urls.paypal}</a></u>                                                        
